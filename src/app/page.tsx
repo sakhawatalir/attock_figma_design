d6a56a05ffa570f44980bbfcc00b76f4138dea1b
sakhawatalir <jam752575@gmail.com>
2025-08-28 12:00:32 +0500

    Update images and deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,7 +179,7 @@ export default function Home() {
         <div className="grid grid-cols-4 gap-0 w-full">
           <div className="w-full h-[458px] overflow-hidden">
             <Image
-              src="/images/01.svg"
+              src="/images/01.webp"
               alt="CUZCO RESTAURANT Sign"
               width={360}
               height={458}
